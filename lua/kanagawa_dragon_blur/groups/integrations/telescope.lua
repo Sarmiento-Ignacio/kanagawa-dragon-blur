13a0b2b54c14a3361964dd6c5b12f762c82c4607
Ignacio Sarmiento <sarmiento.delpozo.i@gmail.com>
2025-06-27 17:37:06 -0400

    fix background

--- lua/kanagawa_dragon_blur/groups/integrations/telescope.lua
+++ lua/kanagawa_dragon_blur/groups/integrations/telescope.lua
@@ -1,13 +1,13 @@
 local p = require("kanagawa_dragon_blur.palette")
 
 return {
-    TelescopeBorder        = { fg = p.bg_dark, bg = p.bg_dark },
-    TelescopeNormal        = { fg = p.fg, bg = p.bg_dark },
-    TelescopePreviewTitle  = { fg = p.bg_dark, bg = p.bg_dark },
-    TelescopeResultsTitle  = { fg = p.bg_dark, bg = p.bg_dark },
-    TelescopePromptTitle   = { fg = p.fg, bg = p.bg_dark, italic = true },
-    TelescopePromptBorder  = { fg = p.gray3, bg = p.bg_dark },
-    TelescopePromptNormal  = { fg = p.fg, bg = p.bg_dark },
-    TelescopePromptCounter = { fg = p.gray4, bg = p.bg_dark },
+    TelescopeBorder        = { fg = p.bg_dark, bg = "NONE" },
+    TelescopeNormal        = { fg = p.fg, bg = "NONE" },
+    TelescopePreviewTitle  = { fg = p.bg_dark, bg = "NONE" },
+    TelescopeResultsTitle  = { fg = p.bg_dark, bg = "NONE" },
+    TelescopePromptTitle   = { fg = p.fg, bg = "NONE", italic = true },
+    TelescopePromptBorder  = { fg = p.gray3, bg = "NONE" },
+    TelescopePromptNormal  = { fg = p.fg, bg = "NONE" },
+    TelescopePromptCounter = { fg = p.gray4, bg = "NONE" },
     TelescopeMatching      = { fg = p.yellow, bold = true },
 }
