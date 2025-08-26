934a01a0fb34d91bb2078cf5eeba67e5d0f7dc05
Sarmiento Ignacio <sarmiento.delpozo.i@gmail.com>
2025-08-26 10:27:27 -0400

    change color syntax

--- lua/kanagawa_dragon_blur/groups/syntax.lua
+++ lua/kanagawa_dragon_blur/groups/syntax.lua
@@ -9,52 +9,46 @@ local styles = vim.tbl_map(function(value)
 end, config.styles)
 
 return {
-	-- =============================================================================
-	-- GRUPOS BÁSICOS DE NEOVIM (usados por TODOS los file managers)
-	-- =============================================================================
-	Directory = { fg = p.blue, bold = true }, -- Carpetas
-	Special = { fg = p.yellow }, -- Archivos especiales (.js, .json, etc.)
-	Type = { fg = p.cyan }, -- Tipos (go, ts, interfaces)
-	Constant = { fg = p.orange }, -- Constantes (rust, toml, config)
-	String = { fg = p.green }, -- Strings (markdown, text, shell)
-	Keyword = { fg = p.purple } + styles.keywords, -- Keywords (c#, php, keywords)
-	Error = { fg = p.red }, -- Errores (java, html)
-	Comment = { fg = p.gray4 } + styles.comments, -- Comentarios (txt, logs)
-	PreProc = { fg = p.magenta }, -- Preprocessor (scss, sass)
-	Identifier = { fg = p.white } + styles.identifiers, -- Identificadores (default)
-	Character = { fg = p.string },
-	Number = { fg = p.number },
-	Float = { fg = p.number },
-	Function = { fg = p.function_ } + styles.functions,
-	Statement = { fg = p.primary },
-	Conditional = { fg = p.keyword },
-	Repeat = { fg = p.keyword },
-	Label = { fg = p.tag },
-	Operator = { fg = p.operator },
-	Exception = { fg = p.keyword },
-	Include = { fg = p.purple },
-	Define = { fg = p.purple },
-	Macro = { fg = p.purple },
-	PreCondit = { fg = p.purple },
-	StorageClass = { fg = p.type },
-	Typedef = { fg = p.type },
-	SpecialChar = { fg = p.string_special },
-	SpecialComment = { fg = p.comment_doc },
-	Debug = { fg = p.hint },
-	Underlined = { underline = true },
-	Todo = { fg = p.variant },
-	Tag = { fg = p.red }, -- Rojo para HTML/Tag
-	Boolean = { fg = p.yellow } + styles.booleans, -- Amarillo para booleanos, lock, svelte, astro
-	Structure = { fg = p.bright_orange }, -- Naranja brillante para enums, struct, event
-
+	Comment = { fg = p.comment, italic = true } + styles.comments, -- any comment
+	Constant = { fg = p.constant }, -- any constant
+	String = { fg = p.string }, -- a string constant
+	Character = { fg = p.string }, -- a character constant
+	Number = { fg = p.number }, -- a number constant
+	Boolean = { fg = p.boolean } + styles.booleans, -- a boolean constant
+	Float = { fg = p.number }, -- a floating point constant
+	Identifier = { fg = p.fg } + styles.identifiers, -- any variable name
+	Function = { fg = p.function_ } + styles.functions, -- function name
+	Statement = { fg = p.primary }, -- any statement
+	Conditional = { fg = p.keyword }, -- if, then, else, etc.
+	Repeat = { fg = p.keyword }, -- for, do, while, etc.
+	Label = { fg = p.tag }, -- case, default, etc.
+	Operator = { fg = p.operator }, -- "+", "*", etc.
+	Keyword = { fg = p.keyword } + styles.keywords, -- any other keyword
+	Exception = { fg = p.keyword }, -- try, catch, throw
+	PreProc = { fg = p.purple }, -- generic Preprocessor
+	Include = { fg = p.purple }, -- preprocessor #include
+	Define = { fg = p.purple }, -- preprocessor #define
+	Macro = { fg = p.purple }, -- same as Define
+	PreCondit = { fg = p.purple }, -- preprocessor #if, #else, etc.
+	Type = { fg = p.type }, -- int, long, char, etc.
+	StorageClass = { fg = p.type }, -- static, register, etc.
+	Structure = { fg = p.enum }, -- struct, union, enum, etc.
+	Typedef = { fg = p.type }, -- A typedef
+	Special = { fg = p.tag }, -- any special symbol
+	SpecialChar = { fg = p.special }, -- special character in a constant
+	Tag = { fg = p.tag }, -- you can use CTRL-] on this
+	SpecialComment = { fg = p.comment_doc }, -- special things inside a comment
+	Debug = { fg = p.hint }, -- debugging statements
+	Underlined = { underline = true }, -- text that stands out
+	Error = { fg = p.variable }, -- any erroneous construct
+	Todo = { fg = p.variant }, -- TODO, FIXME, etc.
 	-- HTML
-	htmlArg = { fg = p.primary, italic = true },
-	htmlEndTag = { fg = p.tag },
-	htmlTitle = { fg = p.title },
-	htmlTag = { fg = p.tag },
+	htmlArg = { fg = p.primary, italic = true }, -- attributes
+	htmlEndTag = { fg = p.tag }, -- end tag />
+	htmlTitle = { fg = p.title }, -- title tag text
+	htmlTag = { fg = p.tag }, -- tag delimiters
 	htmlTagN = { fg = p.tag },
-	htmlTagName = { fg = p.tag },
-
+	htmlTagName = { fg = p.tag }, -- tag text
 	-- Diff
 	diffAdded = { fg = p.string },
 	diffRemoved = { fg = p.variable },
@@ -64,111 +58,4 @@ return {
 	diffFile = { fg = p.comment_doc },
 	diffLine = { fg = p.function_ },
 	diffIndexLine = { fg = p.enum },
-
-	-- =============================================================================
-	-- NETRW (File manager nativo de Neovim)
-	-- =============================================================================
-	netrwDir = { fg = p.blue, bold = true },
-	netrwPlain = { fg = p.white },
-	netrwExe = { fg = p.green, bold = true },
-	netrwSpecial = { fg = p.yellow },
-	netrwSymLink = { fg = p.cyan },
-	netrwTreeBar = { fg = p.gray4 },
-
-	-- =============================================================================
-	-- TELESCOPE (si lo usas como file picker)
-	-- =============================================================================
-	TelescopeResultsSpecialComment = { fg = p.yellow },
-	TelescopeResultsFunction = { fg = p.blue },
-	TelescopeResultsVariable = { fg = p.cyan },
-	TelescopeResultsConstant = { fg = p.orange },
-	TelescopeResultsString = { fg = p.green },
-	TelescopeResultsKeyword = { fg = p.purple },
-
-	-- =============================================================================
-	-- OIL.NVIM (file manager minimalista)
-	-- =============================================================================
-	OilDir = { fg = p.blue, bold = true },
-	OilFile = { fg = p.white },
-	OilCreate = { fg = p.green },
-	OilDelete = { fg = p.red },
-	OilMove = { fg = p.yellow },
-	OilChange = { fg = p.orange },
-
-	-- =============================================================================
-	-- MINI.FILES (del ecosistema mini.nvim)
-	-- =============================================================================
-	MiniFilesDirectory = { fg = p.blue, bold = true },
-	MiniFilesFile = { fg = p.white },
-	MiniFilesBorder = { fg = p.gray4 },
-	MiniFilesTitle = { fg = p.yellow, bold = true },
-
-	-- =============================================================================
-	-- FZF-LUA (si lo usas)
-	-- =============================================================================
-	FzfLuaDir = { fg = p.blue },
-	FzfLuaFile = { fg = p.white },
-
-	-- =============================================================================
-	-- RANGER / LF / NNN (terminal file managers)
-	-- =============================================================================
-	-- Estos usan los colores de terminal, que ya están configurados en terminal.lua
-
-	-- =============================================================================
-	-- CONFIGURACIÓN POR TREESITTER (detecta tipos de archivo automáticamente)
-	-- =============================================================================
-	["@text.uri"] = { fg = p.cyan, underline = true }, -- Links
-	["@text.reference"] = { fg = p.blue }, -- Referencias
-	["@text.title.1"] = { fg = p.red, bold = true }, -- Títulos H1
-	["@text.title.2"] = { fg = p.orange, bold = true }, -- Títulos H2
-	["@text.title.3"] = { fg = p.yellow, bold = true }, -- Títulos H3
-	["@text.emphasis"] = { fg = p.green, italic = true }, -- Énfasis
-	["@text.strong"] = { fg = p.blue, bold = true }, -- Texto fuerte
-	["@markup.heading"] = { fg = p.yellow, bold = true }, -- Headings markdown
-	["@markup.link.url"] = { fg = p.cyan, underline = true }, -- URLs
-	["@markup.raw"] = { fg = p.green }, -- Código raw
-
-	-- =============================================================================
-	-- HIGHLIGHT GROUPS GENÉRICOS PARA EXTENSIONES (algunos plugins los usan)
-	-- =============================================================================
-	FileJs = { fg = p.yellow },
-	FileTs = { fg = p.blue },
-	FileJsx = { fg = p.cyan },
-	FileTsx = { fg = p.cyan },
-	FileCs = { fg = p.purple },
-	FilePy = { fg = p.yellow },
-	FileRs = { fg = p.orange },
-	FileGo = { fg = p.cyan },
-	FileJava = { fg = p.red },
-	FilePhp = { fg = p.purple },
-	FileRb = { fg = p.red },
-	FileLua = { fg = p.blue },
-	FileVim = { fg = p.green },
-	FileC = { fg = p.blue },
-	FileCpp = { fg = p.blue },
-	FileHtml = { fg = p.red },
-	FileCss = { fg = p.blue },
-	FileScss = { fg = p.magenta },
-	FileSass = { fg = p.magenta },
-	FileJson = { fg = p.yellow },
-	FileYaml = { fg = p.purple },
-	FileYml = { fg = p.purple },
-	FileToml = { fg = p.orange },
-	FileXml = { fg = p.green },
-	FileMd = { fg = p.white },
-	FileMarkdown = { fg = p.white },
-	FileTxt = { fg = p.gray4 },
-	FileSh = { fg = p.gray4 },
-	FileBash = { fg = p.gray4 },
-	FileZsh = { fg = p.gray4 },
-	FileFish = { fg = p.green },
-	FilePng = { fg = p.purple },
-	FileJpg = { fg = p.purple },
-	FileJpeg = { fg = p.purple },
-	FileGif = { fg = p.purple },
-	FileSvg = { fg = p.yellow },
-	FileZip = { fg = p.yellow },
-	FileTar = { fg = p.yellow },
-	FileGz = { fg = p.yellow },
-	FileDockerfile = { fg = p.blue },
 }
